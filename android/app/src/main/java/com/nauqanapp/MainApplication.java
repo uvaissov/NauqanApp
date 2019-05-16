119776caedd2d9c00d809b67aba6c5a5236e5567
Serik.Uvaissov <uvaissov@gmail.com>
2019-05-16 21:32:35 +0600

    appcenter fix, trasition fix and flatlist fix

--- android/app/src/main/java/com/nauqanapp/MainApplication.java
+++ android/app/src/main/java/com/nauqanapp/MainApplication.java
@@ -3,9 +3,6 @@ package com.nauqanapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
-import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
-import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import com.horcrux.svg.SvgPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -32,9 +29,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
-            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
-            new AppCenterReactNativePackage(MainApplication.this),
             new SvgPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage(),
